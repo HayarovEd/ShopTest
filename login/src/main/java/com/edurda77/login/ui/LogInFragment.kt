976ebd6312673c887247803c008c2911052c54e8
hayarovy <edurd@rambler.ru>
2023-02-28 14:25:21 +0500

    logic in  login fragment

--- login/src/main/java/com/edurda77/login/ui/LogInFragment.kt
+++ login/src/main/java/com/edurda77/login/ui/LogInFragment.kt
@@ -6,30 +6,59 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import com.edurda77.domain.navigation.Action
+import com.edurda77.domain.navigation.AppNavigation
 import com.edurda77.login.R
+import com.edurda77.login.databinding.FragmentLogInBinding
 import com.edurda77.login.presentation.LogInViewModel
+import com.edurda77.login.presentation.LoginFragmentState
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class LogInFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = LogInFragment()
-    }
-
-    private lateinit var viewModel: LogInViewModel
+    private var _binding: FragmentLogInBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel by viewModels<LogInViewModel>()
+    @Inject
+    lateinit var coordinator: AppNavigation
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_log_in, container, false)
+    ): View {
+        _binding = FragmentLogInBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LogInViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.state.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is LoginFragmentState.Loading -> {
+                }
+                is LoginFragmentState.Success -> {
+                    Toast.makeText(requireContext(), state.data, Toast.LENGTH_LONG).show()
+                    coordinator.execute(
+                        Action.LogInToHome, state.user
+                    )
+                }
+                is LoginFragmentState.Error -> {
+                    Toast.makeText(requireContext(), state.error, Toast.LENGTH_LONG).show()
+                }
+                is LoginFragmentState.Empty -> {
+
+                }
+            }
+        }
+        binding.logInBt.setOnClickListener {
+            viewModel.logInTo(
+                userName = binding.loginEv.text.toString(),
+                password = binding.passwordEv.text.toString()
+            )
+        }
     }
 
 }
